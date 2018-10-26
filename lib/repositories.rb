5f53e326e299da9e1b8e35a01f6fbe1b49dbb7b5
Ian Whitney <whit0694@umn.edu>
2018-10-26 14:23:26 -0500

    Move everything to the Prrr namespace

--- lib/repositories.rb
+++ lib/repositories.rb
@@ -1,28 +1,30 @@
-class Repositories
-  include Enumerable
-  extend Forwardable
-  def_delegators :@members, :each
+module Prrr
+  class Repositories
+    include Enumerable
+    extend Forwardable
+    def_delegators :@members, :each
 
-  def self.build(organization:, included:, client:)
-    if included.any?
-      new(organization, included, client)
-    else
-      all(organization, client)
-    end
-  end
-
-  def initialize(organization, included, client)
-    @members = included.map do |name|
-      if client.repository?("#{organization}/#{name}")
-        client.repository("#{organization}/#{name}")
+    def self.build(organization:, included:, client:)
+      if included.any?
+        new(organization, included, client)
+      else
+        all(organization, client)
       end
     end
-  end
-  private_class_method :new
 
-  def self.all(organization, client)
-    repositories = client.organization_repositories(organization).map { |r| r.name }
-    new(organization, repositories)
+    def initialize(organization, included, client)
+      @members = included.map do |name|
+        if client.repository?("#{organization}/#{name}")
+          client.repository("#{organization}/#{name}")
+        end
+      end
+    end
+    private_class_method :new
+
+    def self.all(organization, client)
+      repositories = client.organization_repositories(organization).map { |r| r.name }
+      new(organization, repositories)
+    end
+    private_class_method :all
   end
-  private_class_method :all
 end
--- lib/reviewer_team.rb
+++ lib/reviewer_team.rb
@@ -1,49 +1,51 @@
-class ReviewerTeam
-  def initialize(organization:, name:, client:)
-    self.organization = organization
-    self.name = name
-    self.client = client
-  end
+module Prrr
+  class ReviewerTeam
+    def initialize(organization:, name:, client:)
+      self.organization = organization
+      self.name = name
+      self.client = client
+    end
 
-  def id
-    attributes.id
-  end
+    def id
+      attributes.id
+    end
 
-  def exists?
-    !attributes.nil?
-  end
+    def exists?
+      !attributes.nil?
+    end
 
-  def has_members?
-    members.any?
-  end
+    def has_members?
+      members.any?
+    end
 
-  def reviewer_for(pull_request:)
-    members.detect { |r| !r.casecmp(pull_request.user.login).zero? }
-  end
+    def reviewer_for(pull_request:)
+      members.detect { |r| !r.casecmp(pull_request.user.login).zero? }
+    end
 
-  def enqueue(member: )
-    members.delete(member)
-    members.push(member)
-  end
+    def enqueue(member: )
+      members.delete(member)
+      members.push(member)
+    end
 
-  def refresh!
-    # Combines the current members with new members and preserves ordering
-    @members = (members | get_members) & get_members
-  end
+    def refresh!
+      # Combines the current members with new members and preserves ordering
+      @members = (members | get_members) & get_members
+    end
 
-  private
+    private
 
-  attr_accessor :organization, :name, :client
+    attr_accessor :organization, :name, :client
 
-  def attributes
-    @attributes ||= client.organization_teams(organization).detect { |team| team.name.casecmp(name).zero? }
-  end
+    def attributes
+      @attributes ||= client.organization_teams(organization).detect { |team| team.name.casecmp(name).zero? }
+    end
 
-  def members
-    @members ||= get_members
-  end
+    def members
+      @members ||= get_members
+    end
 
-  def get_members
-    client.team_members(id).map {|member| member.login }
+    def get_members
+      client.team_members(id).map {|member| member.login }
+    end
   end
 end
